0f21e423ada431c16d8a9f683d2215c762588481
CHIRU-17BCI0152 <44204191+CHIRU3223@users.noreply.github.com>
2020-07-04 11:52:38 +0530

    Create Mini-Max-sum.java

---
+++ Interview Preparation Kit/Mini-Max-sum.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        int min=0,max=0;
+        Arrays.sort(arr);
+        for(int i=0;i<4;i++){
+            min+=arr[i];
+            max+=arr[i+1];
+        }
+        System.out.println(min+" "+max);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
