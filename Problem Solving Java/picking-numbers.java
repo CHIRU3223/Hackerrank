1aba48cc63e4df8203bc113b0f31238ca77b92d8
CHIRU-17BCI0152 <44204191+CHIRU3223@users.noreply.github.com>
2020-07-24 21:02:57 +0530

    Create picking-numbers.java

---
+++ Problem Solving Java/picking-numbers.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+    // Write your code here
+        List<Integer> b=new ArrayList<Integer>();
+        int c;
+        Collections.sort(a,Collections.reverseOrder());
+        for(int i=0;i<a.size();i++){
+            // b.clear();
+            c=1;
+            for(int j=i+1;j<a.size();j++){
+                if(Math.abs(a.get(j)-a.get(i))<=1){
+                    c++;
+                }
+            }
+            b.add(c);
+
+        }
+        Collections.sort(b,Collections.reverseOrder());
+        return b.get(0);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
