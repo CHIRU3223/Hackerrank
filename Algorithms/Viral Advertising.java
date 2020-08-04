46613fb2fac3fba44cbeb19aa58cf3c03fdcfa66
CHIRU-17BCI0152 <44204191+CHIRU3223@users.noreply.github.com>
2020-08-04 12:50:54 +0530

    Create Viral Advertising.java

---
+++ Algorithms/Viral Advertising.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n) {
+        int initial=5;
+        int total_liked=0;
+        for(int i=0;i<n;i++){
+            int liked=initial/2;
+            total_liked+=liked;
+            initial=liked*3;
+        }
+        return total_liked;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
